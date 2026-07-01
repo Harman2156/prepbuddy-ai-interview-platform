22731e759151f82e85a266d46e02b980668f9ce8
Harman Singh <garhiaa2332@gmail.com>
2026-07-01 13:35:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -101,15 +101,10 @@ app.get("/", (req, res) => {
 
 
 // PORT
-const PORT =
-5000;
+const PORT = process.env.PORT || 5000;
 
 
 
 app.listen(PORT, () => {
-
-  console.log(
-
-    `Server running on port ${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
